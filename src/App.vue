8bce1d0e664cbdfb674828f2ca921c25b77b456d
ankit407 <amankit207@gmail.com>
2022-11-12 22:34:32 +0530

    Vue Calculator App

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <CalculatorView></CalculatorView>
+  </div>
+</template>
+
+<script>
+  import CalculatorView from "./components/Calculator.vue";
+  export default {
+    name: "App",
+    components: {
+      CalculatorView,
+    },
+  };
+</script>
+
+<style>
+  body {
+    background: #31475e !important;
+  }
+</style>
